bd1876fde8e1874cd7fad6ca9815f9ba6ae7eba7
quock <quockhanh1382002@gmail.com>
2024-02-29 23:32:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+set up node js
+1. install node js
+2. npm init
+3. npm install express
+4. npm install nodemon --save-dev // auto save
+5. create script in file package : start:nodemon --inspect index.js
+6. npm install morgan --save-dev//add log 
+7. npm i express-handlebars
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// console.log('PATH : ' + path.join(__dirname, 'public'));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+//template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources//views'));
+
+// Routes
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
